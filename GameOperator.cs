8c30ca2337384ce2ee5bc641f21fb75573787dae
Sheng Lee <jason83925@gmail.com>
2024-03-20 17:32:25 +1000

    Add: Gamefile, GameOperator, ProgramEngine, and Board.

--- GameOperator.cs
+++ GameOperator.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata;
+
 namespace Treblecross
 {
     public enum GameMode
@@ -8,9 +10,14 @@ namespace Treblecross
 
     public abstract class GameOperator
     {
+        public abstract string GameId {get;}
         public GameMode Mode { get; }
         private Player[] players = new Player[2];
         private Board board;
+
+        public GameOperator(GameMode mode) {
+            Mode = mode;
+        }
 
         public GameOperator(GameMode mode, Player[] players, Board board)
         {
@@ -23,6 +30,12 @@ namespace Treblecross
             this.board = board;
         }
 
+        public void Init() {
+            // create players
+            // prompt "board size"
+            // create board
+        }
+
         public void Start()
         {
             // looping players
@@ -30,20 +43,53 @@ namespace Treblecross
 
         void makeMove(Player player)
         {
+            // impletement template pattern.
+
             // prompting movement question
             // create Gamestate
         }
 
         protected abstract void makeRandomMove();
-
-
+        protected abstract void validateMove(GameState state);
+        protected abstract void updateBoard(GameState state);
+        protected abstract void determineWinner(GameState state);
+        protected abstract void end();
+        public abstract void GetHint();
     }
 
     public class TreblecrossOperator : GameOperator
     {
+        public override string GameId => "Treblecross";
+
         public TreblecrossOperator(GameMode mode, Player[] players, Board board) : base(mode, players, board) { }
 
+
+        public override void GetHint()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void determineWinner(GameState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void end()
+        {
+            throw new NotImplementedException();
+        }
+
         protected override void makeRandomMove()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void updateBoard(GameState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void validateMove(GameState state)
         {
             throw new NotImplementedException();
         }
